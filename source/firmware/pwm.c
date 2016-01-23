7611c8c5cf8067b725596a2d89dd940ae65d48ef
christakahashi <cnt@u.washington.edu>
2016-01-23 11:58:46 -0800

    updated code to match PCB 3.3

--- source/firmware/pwm.c
+++ source/firmware/pwm.c
@@ -10,28 +10,44 @@
 
 volatile uint16_t epocht;
 volatile uint16_t millist;
+volatile uint16_t ust;
 
 //setup stir motor PWM
 //TODO: convert this to work with timer2
 void pwm_init() {
 	/******STIR INIT*******/
-#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega328__)
-	DDRB |= 0x08; //PB3:OSC2A output
+
 	DDRD |= 0x08; //PD3:OSC2B output
-#elif defined(__AVR_ATmega164A__) || defined(__AVR_ATmega164P__)
-	DDRD |= 0x80 | 0x40;
-#else
-#error "UNSUPPORTED arcitecture.  Cannot continue"
-#endif	
+	DDRD |= 0x04; //PD2 -> !MRST
+	DDRD |= 0x40; //PD6 -> !MEN
+	DDRD |= 0x80; //PD7 -> MDIR
+	DDRB |= 0x01; //PB0 -> !MSLP
+	DDRC |= 0x07; //PC0-PC2 output  -> MS1-MS3
+	/************************************************************************/
+	/*
+	MS1 	MS2 	MS3 	Microstep Resolution
+	Low 	Low 	Low 	Full step
+	High	Low 	Low 	Half step
+	Low 	High	Low 	Quarter step
+	High	High	Low 	Eighth step
+	High	High	High	Sixteenth step                                  */
+	/************************************************************************/
+	PORTC &= ~0x07; //PC0-PC2 low = full step
+	//PORTC |= 0x01; //half step
+	PORTD |= 0x04; //pull motor reset high
+	PORTD &= ~0x40; //pull motor enable low
+	PORTB |= 0x01; //pull motor sleep high
 	
-	//CTC mode, toggle on compare match A, B
-	TCCR2A = 0x02 | 0x40 | 0x10;
-	//cksel /1024, 
-	TCCR2B = 0x07;
 	
-	//toggle on couter top and half counter top to create 90deg phase
+	//CTC mode, toggle on compare match  B
+	TCCR2A = 0x02 | 0x10; 
+	//cksel /256,
+	TCCR2B = 0x06;
+	
+	//toggle on counter top/2  
+	//OCR2A = COUNTER_TOP/2;
 	OCR2A = COUNTER_TOP;
-	OCR2B = COUNTER_TOP/2;
+	OCR2B = 0;
 	
 	TIMSK2 = 1<<OCIE2A; 
 	epocht = 0;
@@ -39,16 +55,16 @@ void pwm_init() {
 }
 
 //one epoch is 2/STIR_SPEED seconds
-ISR (TIMER2_COMPA_vect) {
-	epocht++;	
-  millist += (2000L/STIR_SPEED);
+//takes roughly 120 cycles every US_PER_INT
+ISR (TIMER2_COMPA_vect) { 
+  epocht++;	
+  ust += US_PER_INT;
+  if (ust>1000) {
+	  ust-=1000;
+	  millist +=1;
+  }
 }
 
-//this is a low res estimate of current time
-// it is roughly 2/STIR_SPEED of a second accurate.
-// ie for STIR_SPEED=400 it is accurate to +/- 5 millis
-// Over flows ever minute or so.
-// Note: millis will be off if 2000L/STIR_SPEED has a remainder.
 uint16_t millis() {
   return millist;
 }
--- source/firmware/pwm.h
+++ source/firmware/pwm.h
@@ -8,7 +8,7 @@
 #define PWM_H_
 #include <avr/io.h>
 
-extern uint8_t tmr2ofcnt;
+//extern uint8_t tmr2ofcnt;
 uint16_t millis();
 uint16_t epoch();
 void pwm_init();
@@ -16,15 +16,14 @@ void pwm_init();
 /////STIR/////
 //NOTE: changing STIRS_SPEED so it is not a factor of 2000
 // will break millis().  ie 2000L/STIR_SPEED should have no remainder.
-#define STIR_SPEED 400 //edges per second
 
-//this is defined by the prescalar so don't change this!
-//unit is edges per second (i.e. steps per second)
-//prescalar is 1/1024.  2 edges per timer overflow, 8mhz
-//timer max is 2^8
-//note: >>18 is /(1024*2^8)
-#define MIN_STIR ((2*8000000L)>>18)  //61 edges per second ~ 1.5 rev/sec
-//desired edges per second
-#define COUNTER_TOP (MIN_STIR*(1L<<8)/STIR_SPEED - 1)
+//pulses/sec = 8,000,000/256/2/COUNTER_TOP
+//RPM when in half step mode = (puleses/sec)/96*60
+//=> rpm = 9765.625/COUNTER_TOP
+#define COUNTER_TOP 32
+//interrupts second is:
+// 8,000,000/256/COUNTER_TOP
+
+#define US_PER_INT 1*(COUNTER_TOP+1)*(256/8)
 
 #endif /* PWM_H_ */
